c2607b2bcb619b5e6a5bd561f8dffadf25ee7451
gusainhimanshu <gusainhimanshu@gmail.com>
2013-05-29 22:12:30 +0530

    Update Program3.c
    Added a comment

--- Program3.c
+++ Program3.c
@@ -8,7 +8,7 @@ int main()
     unsigned int num=0;
     printf("Enter the number: ");
     scanf("%d",&n);
-    num=(n&(0xffffff8))+8;
+    num=(n&(0xffffff8))+8; //n&0xffffff8 basically gives the nearest multiple of 8 which is less than n
 
     printf("\n\nThe next multiple of 4 after this number is: %d\n\n", num);
     return 0;
