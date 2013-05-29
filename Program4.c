41ce9800c737a534b870c2629b7a7003075c2a94
gusainhimanshu <gusainhimanshu@gmail.com>
2013-05-29 22:27:10 +0530

    Update Program4.c
    Changes  made regarding making it more flexible and added some comments

--- Program4.c
+++ Program4.c
@@ -19,37 +19,56 @@
 #include <string.h>
 #define MAX 100
 
-
 int main()
 {
 
-    //long int n;
-    char str[MAX];
-    char minstr[MAX];
-    int i=0;
-    int len=0;
-    int length=0;
-    printf("Enter the string : ");
-    scanf("%s",str);
-    len=strlen(str); //finding length of the string
-
-    length=len;
-    strcpy(minstr,str);
-    while((len) != i)
+    unsigned long int n;
+    char str[MAX][MAX];
+    char minstr[MAX][MAX];
+    printf("INPUT\n");
+    scanf("%ld", &n);   //input the number of strings
+    int k=0;
+    /* Taking strings to be processed as input*/
+    for(k; k<n; k++)
     {
-        char c= str[0];
-        int j=0;
-        for(j=0; j< (length); j++)
-        {
-            str[j]=str[j+1];
-        }
-        str[len-1]= c;
-        if(0 > strcmp(str,minstr))
-        {
-            strcpy(minstr,str);
-        }
-        i++;
+        scanf("%s", str[k]);    
     }
-    printf("Minimum str is: %s", minstr);
+    for(k=0; k<n; k++)  //loop for each string
+    {
+        int i=0;
+        int len=0;
+        int length=0;
+        len=strlen(str[k]); //finding length of the string
+
+        length=len;
+        strcpy(minstr[k],str[k]);   //taking a copy of string in minimum string
+        while((len) != i)   //loop for te length of the string
+        {
+            char c= str[k][0];
+            int j=0;
+            //keep on shifting the charcters in order to get minimum string
+            for(j=0; j< (length); j++) 
+            {
+                str[k][j]=str[k][j+1];
+            }
+            str[k][len-1]= c;
+            /*comparing the character with minimum string character*/
+            if(0 > strcmp(str[k],minstr[k]))
+            {
+                strcpy(minstr[k],str[k]);
+            }
+            i++;
+        }
+    }
+    printf("OUTPUT\n");
+    /*Printing the minimum string*/
+    for(k=0; k<n; k++)      
+    {
+        printf("\n");
+        printf("%s", minstr[k]);
+    }
     return 0;
 }
+
+
+
